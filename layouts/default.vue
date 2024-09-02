67b76b2a7b1138f6707095b926764c88e3c8a320
stas_fr <stas240600@yandex.ru>
2024-09-03 00:19:11 +0300

    home page start

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     class="overflow-hidden flex flex-col bg-background text-text min-h-screen text-lg"
   >
     <HeaderComponent />
-    <main class="flex flex-auto my-24">
+    <main class="flex flex-auto my-24 flex-col">
       <slot />
     </main>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
-<template>home page</template>
+<template>
+  <div class="box-border max-w-[1000px] my-0 mx-auto px-4 text-center">
+    <div class="bg-secondary py-2 px-12 rounded mb-8">You're welcome!</div>
+    <div class="flex items-center mb-8">
+      <div class="">
+        <div class="text-4xl">Stanislav Farkash</div>
+        <div class="text-md text-left">Moscow, Russia</div>
+      </div>
+      <img
+        class="rounded-full w-28 ml-4 border-4"
+        src="/assets/img/photo.jpeg"
+        alt="photo"
+      />
+    </div>
+  </div>
+</template>
 
 <script setup></script>
