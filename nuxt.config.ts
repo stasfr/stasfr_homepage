d2dcb0be63d237ecfc2efa85afb0ca41f11f07d9
stas_fr <stas240600@yandex.ru>
2024-09-02 22:31:19 +0300

    install dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+  },
+});
