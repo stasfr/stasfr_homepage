7bb000f94c627b39b88d5951a3383c5383328017
stas_fr <stas240600@yandex.ru>
2024-09-03 21:03:29 +0300

    career timeline full done

--- components/Home/Career/Step.vue
+++ components/Home/Career/Step.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="text-left grid-cols-[30px_auto] grid">
     <div><UIIconsCheck class="w-full h-full" /></div>
-    <div class="justify-items-start px-4 italic">{{ timestampData.year }}</div>
-    <div class="w-1/12 h-full bg-white mx-auto"></div>
-    <div class="pb-4 px-4">
-      <div class="text-xl font-bold">{{ timestampData.title }}</div>
-      <div class="text-base">{{ timestampData.text }}</div>
+    <h3 class="justify-items-start px-4 italic">{{ timestampData.year }}</h3>
+    <div>
+      <hr
+        v-if="timestampData.showLine"
+        class="w-1/12 h-full bg-white mx-auto"
+      />
+    </div>
+    <div class="pb-4 px-4 text-base">
+      <h4 class="text-xl font-bold">{{ timestampData.title }}</h4>
+      <p class="mb-2">{{ timestampData.texts.body }}</p>
+
+      <p v-if="timestampData.texts.site" class="font-bold">Site</p>
+      <a class="mb-2" :href="timestampData.texts.site">{{
+        timestampData.texts.site
+      }}</a>
+
+      <p v-if="timestampData.texts.stack" class="font-bold">Stack</p>
+      <p class="mb-2" v-for="stackTech in timestampData.texts.stack">
+        {{ stackTech }}
+      </p>
     </div>
   </div>
 </template>
--- components/Home/Career/index.vue
+++ components/Home/Career/index.vue
@@ -18,14 +18,44 @@
 <script setup>
 const timeline = ref([
   {
-    year: 2000,
+    year: "2000",
     title: "I was born",
-    text: "I was born and could build a PC with closed eyes.",
+    texts: {
+      body: "I was born and could build a PC with closed eyes (starting from 7 yeard old).",
+    },
+    showLine: true,
   },
   {
-    year: 2024,
-    title: "Im doing this site",
-    text: "im writing this",
+    year: "2021 Spring-Summer",
+    title: "Goglobal",
+    texts: {
+      site: "https://goglobal.world/",
+      stack: [
+        "Vue.js 3",
+        "HTML5, CSS3 (SASS)",
+        "JavaScript (ES6+)",
+        "REST API",
+        "Git, GitHub",
+      ],
+    },
+    showLine: true,
+  },
+  {
+    year: "2022-2024",
+    title: 'PJSC "Rosseti Moscow region"',
+    texts: {
+      body: "Managed staff, wrote macros in VBA to automate business-process, organized the work.",
+      site: "https://rossetimr.ru/",
+    },
+    showLine: true,
+  },
+  {
+    year: "2024",
+    title: "I'm doing this site",
+    texts: {
+      body: "and dream of creating my own product that will make the world a little better (or not a little).",
+    },
+    showLine: false,
   },
 ]);
 </script>
