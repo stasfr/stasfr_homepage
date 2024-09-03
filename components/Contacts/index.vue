8fdd0b2a0e45619b46fc0303e66b8c8f8521309b
stas_fr <stas240600@yandex.ru>
2024-09-03 21:43:05 +0300

    contacts page full done

---
+++ components/Contacts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-around mb-4">
+    <div>{{ props.contact.title }}</div>
+    <a
+      class="rounded bg-primary text-textDark py-2 px-4 hover:bg-btnHover transition-colors duration-300 ease-linear"
+      :href="props.contact.link"
+      >{{ props.contact.btn }}</a
+    >
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  contact: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- pages/Contacts.vue
+++ pages/Contacts.vue
@@ -1,3 +1,17 @@
-<template>contacts page</template>
+<template>
+  <div class="box-border w-[440px] my-0 mx-auto px-4 text-center">
+    <h2
+      class="text-3xl text-left underline decoration-border decoration-4 underline-offset-8 mb-4"
+    >
+      In touch
+    </h2>
+    <Contacts v-for="contact in contacts" :contact="contact" />
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+const contacts = ref([
+  { title: "Telegram", link: "https://t.me/stas_fr", btn: "Message me" },
+  { title: "Github", link: "https://github.com/stasfr", btn: "Follow me" },
+]);
+</script>
