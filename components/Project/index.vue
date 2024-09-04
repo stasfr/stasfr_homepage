0ab70abe8fd54eea32cedac314655590d627ebdc
stas_fr <stas240600@yandex.ru>
2024-09-04 20:17:26 +0300

    project page adaptive

---
+++ components/Project/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="flex flex-col p-4 bg-primary text-textDark rounded text-center space-y-4 w-72"
+  >
+    <h3 class="text-xl font-bold">
+      {{ props.project.title }}
+    </h3>
+    <div class="relative w-full h-40">
+      <NuxtImg
+        :src="props.project.img"
+        class="absolute top-0 left-0 w-full h-full object-cover object-top rounded"
+      />
+    </div>
+    <p>{{ props.project.description }}</p>
+    <a
+      :href="props.project.link"
+      class="bg-secondary mx-auto text-textLight rounded py-2 px-8 hover:bg-btnHover transition-colors duration-300 ease-linear"
+      >Visit</a
+    >
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  project: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box-content max-w-[1000px] mx-auto px-4">
     <div class="grid sm:grid-cols-2 grid-cols-1 gap-8">
-      <Projects v-for="project in projects" :project="project" />
+      <Project v-for="project in projects" :project="project" />
     </div>
   </div>
 </template>
@@ -12,16 +12,19 @@ const projects = ref([
     title: "Saviour Care",
     link: "https://stasfr.github.io/Saviour-Care/",
     img: "/img/projects/saviour_care.png",
+    description: "Adaptive landing (Vanilla HTML, CSS)",
   },
   {
     title: "Wine and Dices",
     link: "https://github.com/stasfr/wad-reborn",
     img: "/img/projects/wad.png",
+    description: "Dream project about Wine and Dice Throne (Vue3, Supabase)",
   },
   {
     title: "Goglobal",
     link: "https://goglobal.world/",
     img: "/img/projects/goglobal.png",
+    description: "Vue3.js SPA, made little components (Vue3, JS)",
   },
 ]);
 </script>
