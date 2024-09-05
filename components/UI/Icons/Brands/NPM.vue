02f7fb8dcaecac8d9930f58ffe1ee1ff46db5dc6
stas_fr <stas240600@yandex.ru>
2024-09-05 13:05:38 +0300

    add brands, start skills page

---
+++ components/UI/Icons/Brands/NPM.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    class="w-6 h-6 text-textDark"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 3.87H4v16h8v-13h5v13h3v-16h-8Z"
+    />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  dark: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
--- pages/Skills.vue
+++ pages/Skills.vue
@@ -1,3 +1,54 @@
-<template>skills page</template>
+<template>
+  <section class="grid-cols-4 gap-4 grid">
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsVue />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsCSS />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsHTML />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsGitHub />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsGoogle />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsNPM />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsTailwind />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsNuxt />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsJS />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsTS />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsPostgreSQL />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsPostman />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsSASS />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsNode />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsExpress />
+    </div>
+    <div class="p-4 bg-primary rounded">
+      <UIIconsBrandsGraphQL />
+    </div>
+  </section>
+</template>
 
 <script setup></script>
