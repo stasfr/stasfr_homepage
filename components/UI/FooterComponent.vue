60fccd3640b8ba33b2d19b9ad66fc4c811ca0766
stas_fr <stas240600@yandex.ru>
2024-09-08 20:24:17 +0300

    add footer

---
+++ components/UI/FooterComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="border-t-[1px] border-border">
+    <div class="py-8 max-w-lg mx-auto text-center text-gray-500">
+      Stanislav Farkash, Moscow, 2024
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,11 @@
   <div
     class="overflow-hidden flex flex-col bg-background text-textLight min-h-screen"
   >
-    <HeaderComponent />
-    <main class="my-24 box-content max-w-lg mx-auto px-4">
+    <UIHeaderComponent />
+    <main class="box-content max-w-lg mx-auto px-4 my-24 min-h-[70vh]">
       <slot />
     </main>
+    <UIFooterComponent />
   </div>
 </template>
 
