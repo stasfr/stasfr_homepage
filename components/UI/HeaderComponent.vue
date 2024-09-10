893c035f7031b3e79b1e372b1ce583260f16e2a2
stas_fr <stas240600@yandex.ru>
2024-09-10 22:05:36 +0300

    burger menu start

--- components/UI/HeaderComponent.vue
+++ components/UI/HeaderComponent.vue
@@ -4,15 +4,72 @@
       class="flex min-h-12 items-center justify-between box-content max-w-lg mx-auto px-4"
     >
       <nuxt-link to="/" class="font-bold">stas_fr</nuxt-link>
-      <nav>
+      <nav class="hidden md:block">
         <ul class="flex flex-wrap space-x-4">
           <li><nuxt-link to="/projects">Projects</nuxt-link></li>
           <li><nuxt-link to="/skills">Skills</nuxt-link></li>
           <li><nuxt-link to="/contacts">Contacts</nuxt-link></li>
         </ul>
       </nav>
+      <div class="flex gap-3">
+        <label
+          class="btn btn-circle swap swap-rotate border-none bg-inherit md:hidden text-textLight"
+        >
+          <input
+            v-model="checkbox"
+            type="checkbox"
+            @click="mobileMenu = !mobileMenu"
+          />
+          <UIIconsBurger />
+          <UIIconsClose />
+        </label>
+      </div>
+      <transition name="fade"
+        ><div
+          v-if="mobileMenu"
+          class="fixed right-4 top-16 w-full max-w-[150px] rounded-md"
+        >
+          <ul class="flex flex-col gap-1">
+            <nuxt-link
+              @click="(checkbox = !checkbox), (mobileMenu = !mobileMenu)"
+              to="/blog"
+            >
+              <li class="p-2">
+                <span>Blog</span>
+              </li>
+            </nuxt-link>
+            <nuxt-link
+              @click="(checkbox = !checkbox), (mobileMenu = !mobileMenu)"
+              to="/works"
+            >
+              <li class="p-2">
+                <span>Works</span>
+              </li>
+            </nuxt-link>
+            <nuxt-link
+              @click="(checkbox = !checkbox), (mobileMenu = !mobileMenu)"
+              to="/skills"
+            >
+              <li class="p-2">
+                <span>Skills</span>
+              </li>
+            </nuxt-link>
+            <nuxt-link
+              @click="(checkbox = !checkbox), (mobileMenu = !mobileMenu)"
+              to="/contact"
+            >
+              <li class="p-2">
+                <span>Contact</span>
+              </li>
+            </nuxt-link>
+          </ul>
+        </div>
+      </transition>
     </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+const mobileMenu = ref(false);
+const checkbox = ref(false);
+</script>
---
+++ components/UI/Icons/Burger.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    class="swap-off fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="currentColor"
+      d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"
+    />
+  </svg>
+</template>
