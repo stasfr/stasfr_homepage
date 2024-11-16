ae5d1830fc14a0ce522a7e6da703b51e196c2c8f
stasfr <sfworking@yandex.ru>
2024-11-16 15:35:24 +0300

    feat: theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/router/router.tsx";
 
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
