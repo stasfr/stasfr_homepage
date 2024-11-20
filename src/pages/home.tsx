41c219809200d79816732779ff03b2116890709c
stasfr <sfworking@yandex.ru>
2024-11-21 00:19:14 +0300

    types: type safety to HomePageArticles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,18 +3,19 @@ import Avatar from "@/components/Home/Avatar";
 import Article from "@/components/Home/Article";
 
 import HomePageArticles from "@/static/HomePageArticles";
+import type { HomePageArticle } from "@/types/HomePageArticle";
 
 export default function Home() {
   return (
     <section className="space-y-8">
-      <div className="bg-primary-bg xs:w-80 mx-auto flex w-72 items-center justify-center gap-2 rounded-sm py-2 font-medium text-black">
+      <div className="mx-auto flex w-72 items-center justify-center gap-2 rounded-sm bg-primary-bg py-2 font-medium text-black xs:w-80">
         <Icon icon="lucide:heart" className="size-5" />
         From Russian frontend with love!
       </div>
 
       <Avatar />
 
-      {HomePageArticles.map((article) => (
+      {HomePageArticles.map((article: HomePageArticle) => (
         <Article key={article.id} article={article} />
       ))}
     </section>
