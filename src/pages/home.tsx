06d60e83e698db325fd3ccc24f7cb3e18c865325
stasfr <sfworking@yandex.ru>
2024-11-20 17:21:30 +0300

    feat: home page greeting, avatar component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,15 @@
+import { Icon } from "@iconify/react";
+import Avatar from "@/components/Home/Avatar";
+
 export default function Home() {
   return (
-    <>
-      <div>Home Page</div>
-    </>
+    <section className="space-y-8">
+      <div className="bg-primary-bg xs:w-80 mx-auto flex w-72 items-center justify-center gap-2 rounded-sm py-2 font-medium text-black">
+        <Icon icon="lucide:heart" className="size-5" />
+        From Russian frontend with love!
+      </div>
+
+      <Avatar />
+    </section>
   );
 }
