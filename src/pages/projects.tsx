d3942e22f42594d6d33a3988eb559e3545bde5d2
stasfr <sfworking@yandex.ru>
2024-11-20 23:56:32 +0300

    feat: projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,20 @@
+import ProjectCard from "@/components/Projects/ProjectCard";
+import ProjectsStatic from "@/static/ProjectsStatic";
+
+import type { Project } from "@/types/Project";
+
 export default function Projects() {
   return (
-    <>
-      <div>Projects Page</div>
-    </>
+    <section className="mx-auto space-y-8 px-4">
+      <h2 className="text-center text-3xl underline decoration-border decoration-2 underline-offset-8">
+        My projects
+      </h2>
+
+      <div className="grid grid-cols-2 justify-items-center gap-12">
+        {ProjectsStatic.map((project: Project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}{" "}
+      </div>
+    </section>
   );
 }
---
+++ src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  id: number;
+  title: string;
+  link: string;
+  img: string;
+  description: string;
+  stack: string;
+  category?: string;
+}
