bc857b8524e1b6180bae71714e2e84bc75a0243e
stas_fr <sfworking@yandex.ru>
2025-03-24 16:40:18 +0300

    feat: add basic pages

---
+++ pages/projects.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Projects page</div>
+</template>
---
+++ pages/skills.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Skills page</div>
+</template>
