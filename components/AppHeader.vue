b08d8d4e1f55c7393f487708b2711baa485511b2
stas_fr <sfworking@yandex.ru>
2025-05-24 00:21:47 +0300

    refactor: use nuxt-link instead of router push

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-const router = useRouter();
-
-function goTo(path: string) {
-  router.push(path);
-}
-
 function toggleMenu(event: MouseEvent) {
   if (!menu.value) {
     return;
@@ -32,24 +26,26 @@ const items = ref([
 <template>
   <div class="bg-neutral-800">
     <div class="container mx-auto flex justify-between items-center p-2">
-      <PrimeButton
-        class="p-0!"
-        label="stas_fr dev"
-        component="h1"
-        variant="link"
-        @click="goTo('/')" />
+      <NuxtLink to="/">
+        <PrimeButton
+          class="p-0!"
+          label="stas_fr dev"
+          component="h1"
+          variant="link" />
+      </NuxtLink>
 
       <nav class="hidden md:block">
-        <PrimeButton label="Skills" variant="link" @click="goTo('/skills')" />
+        <NuxtLink to="/skills">
+          <PrimeButton label="Skills" variant="link" />
+        </NuxtLink>
 
-        <PrimeButton
-          label="Projects"
-          variant="link"
-          @click="goTo('/projects')" />
-        <PrimeButton
-          label="Contacts"
-          variant="link"
-          @click="goTo('/contacts')" />
+        <NuxtLink to="/projects">
+          <PrimeButton label="Projects" variant="link" />
+        </NuxtLink>
+
+        <NuxtLink to="/contacts">
+          <PrimeButton label="Contacts" variant="link" />
+        </NuxtLink>
       </nav>
 
       <PrimeButton class="md:hidden! p-2!" type="button" @click="toggleMenu">
@@ -58,12 +54,12 @@ const items = ref([
 
       <PrimeMenu id="overlay_menu" ref="menu" :model="items" :popup="true">
         <template #item="{ item, props }">
-          <router-link v-slot="{ href, navigate }" :to="item.route" custom>
+          <NuxtLink v-slot="{ href, navigate }" :to="item.route" custom>
             <a :href="href" v-bind="props.action" @click="navigate">
               <span :class="item.icon" />
               <span class="ml-2">{{ item.label }}</span>
             </a>
-          </router-link>
+          </NuxtLink>
         </template>
       </PrimeMenu>
     </div>
