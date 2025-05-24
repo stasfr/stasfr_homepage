06e00c254a5dbc1ccfbd2d81c5eb13894963dc85
stas_fr <sfworking@yandex.ru>
2025-05-24 13:15:13 +0300

    feat: add link button component

---
+++ components/contacts/UI/LinkButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  href: string;
+  icon: string;
+  text: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <a class="font-thin tracking-widest" :href="props.href">
+    <PrimeButton class="!p-2 md:!hidden">
+      <Icon :name="props.icon" class="!w-6 !h-6" />
+    </PrimeButton>
+
+    <span class="hidden md:block">{{ props.text }}</span>
+  </a>
+</template>
