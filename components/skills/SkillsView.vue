075313c78890d1f3f686206f341d4ab6f62c4f99
stas_fr <sfworking@yandex.ru>
2025-05-29 23:26:02 +0300

    fix: color change on hover on mobile devices

--- components/skills/SkillsView.vue
+++ components/skills/SkillsView.vue
@@ -3,144 +3,145 @@ const skills = ref([
   {
     name: 'flowbite:css-solid',
     title: 'CSS',
-    hoverClass: 'hover:text-[#3C99DC]',
+    color: '#3C99DC',
   },
   {
     name: 'simple-icons:express',
     title: 'Express',
+    color: '#ffffff',
   },
-  { name: 'bi:git', title: 'Git', hoverClass: 'hover:text-[#F1502F]' },
+  { name: 'bi:git', title: 'Git', color: '#F1502F' },
   {
     name: 'akar-icons:google-fill',
     title: 'Google',
-    hoverClass: 'hover:text-[#4285F4]',
-    // hoverClass: 'hover:text-[#FBBC05]',
-    // hoverClass: 'hover:text-[#34A853]',
-    // hoverClass: 'hover:text-[#EA4335]',
+    color: '#4285F4',
+    // color: '#FBBC05',
+    // color: '#34A853',
+    // color: '#EA4335',
   },
   {
     name: 'akar-icons:graphql-fill',
     title: 'GraphQL',
-    hoverClass: 'hover:text-[#E10098]',
+    color: '#E10098',
   },
   {
     name: 'flowbite:html-solid',
     title: 'HTML',
-    hoverClass: 'hover:text-[#F06529]',
+    color: '#F06529',
   },
   {
     name: 'akar-icons:javascript-fill',
     title: 'JS',
-    hoverClass: 'hover:text-[#F0DB4F]',
+    color: '#F0DB4F',
   },
   {
     name: 'akar-icons:node-fill',
     title: 'Node.js',
-    hoverClass: 'hover:text-[#339933]',
+    color: '#339933',
   },
   {
     name: 'akar-icons:npm-fill',
     title: 'NPM',
-    hoverClass: 'hover:text-[#CC3534]',
+    color: '#CC3534',
   },
   {
     name: 'simple-icons:nuxtdotjs',
     title: 'Nuxt',
-    hoverClass: 'hover:text-[#00dc82]',
+    color: '#00dc82',
   },
   {
     name: 'akar-icons:postgresql-fill',
     title: 'PostgreSQL',
-    hoverClass: 'hover:text-[#336791]',
+    color: '#336791',
   },
   {
     name: 'simple-icons:postman',
     title: 'Postman',
-    hoverClass: 'hover:text-[#EF5B25]',
+    color: '#EF5B25',
   },
   {
     name: 'akar-icons:sass-fill',
     title: 'SASS',
-    hoverClass: 'hover:text-[#CD6799]',
+    color: '#CD6799',
   },
   {
     name: 'flowbite:tailwind-solid',
     title: 'Tailwind',
-    hoverClass: 'hover:text-sky-400',
+    color: '#00bcff',
   },
   {
     name: 'akar-icons:typescript-fill',
     title: 'TS',
-    hoverClass: 'hover:text-[#3178c6]',
+    color: '#3178c6',
   },
   {
     name: 'akar-icons:vue-fill',
     title: 'Vue',
-    hoverClass: 'hover:text-[#42b883]',
+    color: '#42b883',
   },
   {
     name: 'file-icons:chartjs',
     title: 'Chart.js',
-    hoverClass: 'hover:text-[#ff6384]',
+    color: '#ff6384',
   },
   {
     name: 'ant-design:docker-outlined',
     title: 'Docker',
-    hoverClass: 'hover:text-[#1D63ED]',
+    color: '#1D63ED',
   },
   {
     name: 'ri:supabase-fill',
     title: 'Supabase',
-    hoverClass: 'hover:text-[#3bce8d]',
+    color: '#3bce8d',
   },
   {
     name: 'akar-icons:figma-fill',
     title: 'Figma',
-    hoverClass: 'hover:text-[#f24e1e]',
-    // hoverClass: 'hover:text-[#ff7262]',
-    // hoverClass: 'hover:text-[#a259ff]',
-    // hoverClass: 'hover:text-[#1abcfe]',
-    // hoverClass: 'hover:text-[#0acf83]',
+    color: '#f24e1e',
+    // color: '#ff7262',
+    // color: '#a259ff',
+    // color: '#1abcfe',
+    // color: '#0acf83',
   },
   {
     name: 'simple-icons:nestjs',
     title: 'Nest.js',
-    hoverClass: 'hover:text-[#E0234E]',
+    color: '#E0234E',
   },
   {
     name: 'simple-icons:primevue',
     title: 'PrimeVue',
-    hoverClass: 'hover:text-[#42b883]',
+    color: '#42b883',
   },
   {
     name: 'devicon-plain:vuetify',
     title: 'Vuetify',
-    hoverClass: 'hover:text-[#1697F6]',
+    color: '#1697F6',
   },
   {
     name: 'simple-icons:quasar',
     title: 'Quasar',
-    hoverClass: 'hover:text-[#00b4ff]',
+    color: '#00b4ff',
   },
   {
     name: 'simple-icons:prisma',
     title: 'Prisma',
-    hoverClass: 'hover:text-[#4c51bf]',
+    color: '#4c51bf',
   },
   {
     name: 'simple-icons:mongodb',
     title: 'MongoDB',
-    hoverClass: 'hover:text-[#00ed64]',
+    color: '#00ed64',
   },
   {
     name: 'simple-icons:gitlab',
     title: 'GitLab',
-    hoverClass: 'hover:text-[#fc6d27]',
+    color: '#fc6d27',
   },
   {
     name: 'simple-icons:vitest',
     title: 'Vitest',
-    hoverClass: 'hover:text-[#fcc72b]',
+    color: '#fcc72b',
   },
 ]);
 
@@ -167,13 +168,15 @@ function shuffleSkills() {
       <TransitionGroup
         name="list-shuffle"
         tag="div"
-        class="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        class="grid grid-cols-3 sm:grid-cols-4 gap-4">
         <div
           v-for="skill in skills"
           :key="skill.name"
-          class="hover:scale-110 transition-all duration-300 hover:bg-zinc-700 flex gap-2 rounded-lg bg-zinc-800 p-2 size-20 items-center justify-center"
-          :class="[skill.hoverClass]">
-          <Icon :name="skill.name" class="!size-12" />
+          class="skill-item hover:scale-110 active:scale-110 transition-all duration-300 active:bg-zinc-700 hover:bg-zinc-700 flex gap-2 rounded-lg bg-zinc-800 p-2 size-20 items-center justify-center"
+          :style="{ '--skill-icon-color': skill.color }">
+          <Icon
+            :name="skill.name"
+            class="!size-12 skill-icon-dynamic-color transition-colors duration-300" />
         </div>
       </TransitionGroup>
     </div>
@@ -183,5 +186,9 @@ function shuffleSkills() {
 <style>
 .list-shuffle-move {
   transition: transform 0.8s ease;
+}
+.skill-item:hover .skill-icon-dynamic-color,
+.skill-item:active .skill-icon-dynamic-color {
+  color: var(--skill-icon-color);
 }
 </style>
